ae23af75437fda15857de9730cf52a1856f1b8b3
mfloreszarate <74306486+mfloreszarate@users.noreply.github.com>
2024-12-27 13:38:43 -0300

    fix

--- src/pages/projects/item/ProjectItem.tsx
+++ src/pages/projects/item/ProjectItem.tsx
@@ -11,7 +11,7 @@ export const ProjectItem = ({ name, backgroundImage, images }: ProjectProps) => 
 
   const [isOpen, setIsOpen] = useState(false)
 
-  const handleClose = (event: any) => {
+  const handleClose = () => {
     setIsOpen(false)
   }
 
