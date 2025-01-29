dcc8f596c606de6aacff23f7ba90ca7bb51e5758
mfloreszarate <74306486+mfloreszarate@users.noreply.github.com>
2025-01-29 17:34:02 -0300

    avances

--- src/components/layout/navbar/ResponsiveNavbar.tsx
+++ src/components/layout/navbar/ResponsiveNavbar.tsx
@@ -1,23 +1,27 @@
 import { useState } from 'react';
 import './navbar.css';
 import { menuConfig } from '../../../utils/menu.config';
+import { useNavigate } from 'react-router-dom';
+
+import Logo from './../../../assets/img/logo-corto.png'
 
 const HamburgerMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const navigate = useNavigate()
 
   const toggleMenu = () => {
     setIsOpen(!isOpen)
   }
 
   const handleClick = (link: string) => {
-    const element = document.getElementById(link)
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
-      setIsOpen(false)
-    }
+    navigate(`/#${link}`)
+    toggleMenu()
   }
+
   return (
-    <div className="hamburger-menu">
+    // <div className='navbar-wrapper' style={{ position: mainPage ? 'fixed' : 'relative', width: '100%', zIndex: 1000, background: backgroundColor }}></div>
+    <div className="hamburger-menu" style={{ position: 'fixed', width: '100%', zIndex: 1000 }}>
       <div className='hamburger-menu-wrapper'>
         <button className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
           <span className="line"></span>
@@ -29,9 +33,14 @@ const HamburgerMenu = () => {
       <nav className={`hamburger-menu-item ${isOpen ? 'show' : ''}`}>
         {/* Botón de cerrar */}
         <button className="close-button" onClick={toggleMenu}>X</button>
-        <ul>
-          {menuConfig.map(el => <li key={el.item} className='menu-item' onClick={() => { handleClick(el.link) }}>{el.item}</li>)}
-        </ul>
+        <div className='ul-wrapper'>
+          <span>
+            <img src={Logo} alt="odin-logo" />
+          </span>
+          <ul>
+            {menuConfig.map(el => <li key={el.item} className='menu-item' onClick={() => { handleClick(el.link) }}>{el.item}</li>)}
+          </ul>
+        </div>
       </nav>
     </div >
   );
